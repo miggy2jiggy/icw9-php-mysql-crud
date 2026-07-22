eb02b1ea5d21b8abdc28c12ba71b9fc668c6199e
Marcus Santiago <112112284+miggy2jiggy@users.noreply.github.com>
2026-07-22 19:52:38 -0400

    Add files via upload
    Set up MySQL database connection.

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "msantiago15";
+$pass = "msantiago15";
+$db = "msantiago15";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
